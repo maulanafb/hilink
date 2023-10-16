1c9db4a01c2874f6f75ee8e61df0c037aee15df6
unknown <gagas112212@gmail.com>
2023-10-16 19:39:33 +0700

    menu mobile

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
 import React from 'react'
+type FunctionComponent<P> = (props: P) => JSX.Element
+
 type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
     variant: string;
     full?: boolean;
+    onClick?: FunctionComponent<{}>
 }
-const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full, onClick }: ButtonProps) => {
     return (
-        <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>{icon && <Image src={icon} alt={title} width={24} height={24} />}
+        <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} onClick={onClick}>{icon && <Image src={icon} alt={title} width={24} height={24} />}
             <label className='bold-16 whitespace-nowrap'>{title}</label>
         </button>
     )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -5,13 +6,20 @@ import { NAV_LINKS } from '@/constants'
 import Button from './Button'
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = React.useState(false)
+
+    const handleMenuClick = () => {
+        setMenuOpen(!menuOpen)
+        console.log('di klik')
+    }
+
     return (
         <nav className=' flexBetween max-container padding-container relative z-30 py-5'>
             <Link href='/'>
                 <Image src='/hilink-logo.svg' alt='logo' width={74} height={29} />
 
             </Link>
-            <ul className='hidden h-full gap-12 lg:flex'>
+            <ul className={`hidden h-full gap-12 lg:flex transition-all ${menuOpen ? 'block' : 'hidden'}`}>
                 {NAV_LINKS.map((link) => (
                     <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>{link.label}</Link>
                 ))}
@@ -19,7 +27,25 @@ const Navbar = () => {
             <div className='lg:flexCenter hidden'>
                 <Button type="button" title='Login' icon='/user.svg' variant="btn_dark_green" />
             </div>
-            <Image className='md:hidden cursor-pointer' src={`/menu.svg`} alt='menu' width={32} height={32} />
+            <div className='lg:hidden'>
+                <Image src={`/menu.svg`} alt='menu' width={32} height={32} onClick={handleMenuClick} />
+            </div>
+            <div
+                className={`absolute w-screen h-screen bg-white right-0 top-0 flex justify-center items-center text-center ${menuOpen ? 'block' : 'hidden'}`}
+            >
+                <ul className='list-disc'>
+                    {NAV_LINKS.map((link) => (
+                        <li key={link.key} className='flex items-center justify-center '>
+                            <Link href={link.href} className='regular-16 text-black cursor-pointer pb-3 transition-all hover:font-bold'>{link.label}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {menuOpen && (
+                <div className='absolute right-0 top-0 mt-4 mr-4'>
+                    <button className='px-5 py2 border-[1px] border-black rounded-3xl' onClick={handleMenuClick}>X</button>
+                </div>
+            )}
         </nav>
     )
 }
